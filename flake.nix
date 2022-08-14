a4a69da855fd79257b684b29f0a405c63e81da7d
diegodox <android.mxdiego9@gmail.com>
2022-08-15 01:33:33 +0900

    add home-manager

--- flake.nix
+++ flake.nix
@@ -1,17 +1,16 @@
 {
-  description = "My NixOS configuration";
+  description = "My NixOS and stand-alone home-manager configuration";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  # outputs = { nixpkgs, home-manager, ... }:
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -21,11 +20,33 @@
     };
     lib = nixpkgs.lib;
   in {
+    # Stand-alone home-manager flake
+    #
+    # Usage:
+    #   1. Install Home Manager and apply the configuration by 
+    #     $ nix build --no-link .#homeConfiguration.diego.activationPacakge
+    #     $ "$(nix path-info .#homeConfiguration.diego.activationPackage"/activate
+    #
+    #   2. Once home-manage is installed.
+    #     $ home-manager switch --flake '.#diego'
+    #     to apply.
+    #
+    # See: [Home Manager Manual / 3 Nix Flakes / 3.2 Standalone setup](https://nix-community.github.io/home-manager/index.html#sec-flakes-standalone)
+    homeConfigurations.diego = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home/diego.nix  ];
+    };
+
     nixosConfigurations = {
       nixos-vm = lib.nixosSystem {
         inherit system;
         modules = [
           ./nixos-vm/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.diego = import ./home/diego.nix;
+          }
         ];
       };
     };
---
+++ home/diego.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  # programs.home-manager.enable = true;
+  home.username = "diego";
+  home.homeDirectory = "/home/diego";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manger release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+}
